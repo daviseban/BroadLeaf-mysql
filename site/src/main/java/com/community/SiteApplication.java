bd3aaa2c7e3ed91bb69edd56d1c4991fe2d74057
daviseban <daviseban@gmail.com>
2017-12-12 19:16:42 +0530

    Update SiteApplication.java

--- site/src/main/java/com/community/SiteApplication.java
+++ site/src/main/java/com/community/SiteApplication.java
@@ -1,6 +1,9 @@
 package com.community;
 
 import org.broadleafcommerce.common.config.EnableBroadleafSiteAutoConfiguration;
+import org.broadleafcommerce.common.web.boot.support.BroadleafBootServletContextInitializer;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-public class SiteApplication {
+public class SiteApplication extends BroadleafBootServletContextInitializer {
 
     @Configuration
     @EnableBroadleafSiteAutoConfiguration
@@ -16,6 +19,11 @@ public class SiteApplication {
     
     public static void main(String[] args) {
         SpringApplication.run(SiteApplication.class, args);
+    }
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SiteApplication.class);
     }
     
 }
