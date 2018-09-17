29fb3a3644361e16fe58efd0aeec615decd35435
daviseban <daviseban@gmail.com>
2018-09-17 17:16:20 +0530

    deleting

--- before_install.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Clean up the old version of app otherwise, there might be an issue
-[ -d /var/lib/tomcat8/webapps/mycompany ] && rm -rf /var/lib/tomcat8/webapps/mycompany
-[ -f /var/lib/tomcat8/webapps/mycompany.war ] && rm -rf /var/lib/tomcat8/webapps/mycompany.war
-[ $? -eq 0 ] && exit 0
